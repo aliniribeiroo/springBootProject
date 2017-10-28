20cdd5321c0c9ba217a8b439232c070e75d77f2e
Alini <aliniribeiroo@gmail.com>
2017-10-28 20:35:40 -0200

    versão inicial do projeto

---
+++ src/test/java/com/aliniribeiro/api/SpringBootFirstProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aliniribeiro.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootFirstProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
