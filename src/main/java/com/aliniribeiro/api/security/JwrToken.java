8cc2200ba1e3d27aafef36e61cad959f6fd35f67
Alini <aliniribeiroo@gmail.com>
2017-11-09 09:53:43 -0200

    Adicionando profile em component

--- src/main/java/com/aliniribeiro/api/security/JwrToken.java
+++ src/main/java/com/aliniribeiro/api/security/JwrToken.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
+@Profile("prod")
 public class JwrToken {
 
 	static final String CLAIM_KEY_USERNAME = "sub";
